b78cbb1eb546cb1b3ff66e4d15a32d2b3d3c3689
Kimberly Dungaya <kimberlydungaya@Air-de-Kimberly.home>
2024-10-01 14:15:30 +0200

    k

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -5,12 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kimberlydungaya <kimberlydungaya@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/29 15:53:33 by kimberlydun       #+#    #+#             */
-/*   Updated: 2024/09/30 01:15:30 by kimberlydun      ###   ########.fr       */
+/*   Created: 2024/09/29 15:53:33 by kimberlydun       #+#    #+#             *//*   Updated: 2024/09/30 01:15:30 by kimberlydun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//include <libft.h>
+#include "libft.h"
 #include <string.h>
 
 void *ft_memset(void *s, int c, size_t n)
