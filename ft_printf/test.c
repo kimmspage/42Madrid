58759fd23bcfe896d898ced15dd88e072fe684de
Kimberly Dungaya <kimberlydungaya@MacBook-Air-de-Kimberly.local>
2025-03-28 17:59:05 +0100

    :)

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -6,7 +6,7 @@
 /*   By: kimberlydungaya <kimberlydungaya@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 17:31:14 by kimberlydun       #+#    #+#             */
-/*   Updated: 2025/03/28 13:35:28 by kimberlydun      ###   ########.fr       */
+/*   Updated: 2025/03/28 17:57:04 by kimberlydun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,39 +29,40 @@ void comparar_resultados(const char* descripcion, const char* formato, ...) {
     printf("%s - ft_printf len: %d, printf len: %d\n", descripcion, ft_len, printf_len);
 }
 
-#define PRUEBA(formato, ...) comparar_resultados("Prueba", formato, ##__VA_ARGS__)
+#define pruebita(formato, ...) comparar_resultados("Prueba", formato, ##__VA_ARGS__)
 
 int main(void)
 {
     /* Pruebas básicas */
-    PRUEBA("Carácter: %c\n", 'A');
-    PRUEBA("Cadenita normal: %s\n", "Holis Corrector");
-    PRUEBA("Puntero válido: %p\n", (void*)&main);
-    PRUEBA("Entero positivo: %d\n", 42);
-    PRUEBA("Unsigned máximo valor: %u\n", 4294967295U);
-    PRUEBA("Hexadecimal minúsculas: %x\n", 255);
-    PRUEBA("Porcentaje simple: %%\n");
+    printf("\n--- Pruebas Básicas ---\n");
+    pruebita("Carácter: %c\n", 'A');
+    pruebita("Cadenita normal: %s\n", "Holis Corrector");
+    pruebita("Puntero válido: %p\n", (void*)&main);
+    pruebita("Entero positivo: %d\n", 42);
+    pruebita("Unsigned máximo valor: %u\n", 4294967295U);
+    pruebita("Hexadecimal minúsculas: %x\n", 255);
+    pruebita("Porcentaje simple: %%\n");
 
     /* Casos límite */
     printf("\n--- Casos límite ---\n");
-    PRUEBA("Carácter nulo: %c.\n", '\0');
-    PRUEBA("Cadenita vacía: '%s'\n", "");
-    PRUEBA("Cadenita nula: '%s'\n", NULL);
-    PRUEBA("Mínimo entero (INT_MIN): %d\n", -2147483648);
-    PRUEBA("Máximo entero (INT_MAX): %d\n", 2147483647);
-    PRUEBA("Hexadecimal de cero: %x.\n", 0);
+    pruebita("Carácter nulo: %c.\n", '\0');
+    pruebita("Cadenita vacía: '%s'\n", "");
+    pruebita("Cadenita nula: '%s'\n", NULL);
+    pruebita("Mínimo entero (INT_MIN): %d\n", -2147483648);
+    pruebita("Máximo entero (INT_MAX): %d\n", 2147483647);
+    pruebita("Hexadecimal de cero: %x.\n", 0);
 
     /* Mezclita de especificadores */
     printf("\n--- Mezclita de especificadores ---\n");
-    PRUEBA("Carácter: '%c', Cadena: '%s', Entero negativo: '%d', Hexa Mayús: '%X', Porcentaje literal '%%'\n",
+    pruebita("Carácter: '%c', Cadena: '%s', Entero negativo: '%d', Hexa Mayús: '%X', Porcentaje literal '%%'\n",
         'Z', "Pruebita Mixta!", -4242, 4242);
 
     /* Casos adicionales */
     printf("\n--- Casos adicionales ---\n");
-    PRUEBA("Unsigned máximo + 1: %u\n", 4294967296U);
-    PRUEBA("Hexadecimal de número negativo: %x\n", -42);
-    PRUEBA("Puntero NULL: %p\n", NULL);
-    PRUEBA("Cadena larga: %s\n", "Esta es una cadena extremadamente larga para probar el manejo de cadenas extensas en la función ft_printf...");
+    pruebita("Unsigned máximo + 1: %u\n", 4294967296U);
+    pruebita("Hexadecimal de número negativo: %x\n", -42);
+    pruebita("Puntero NULL: %p\n", NULL);
+    pruebita("Cadena larga: %s\n", "Esta es una cadena extremadamente larga para probar el manejo de cadenas extensas en la función ft_printf...");
 
     return (0);
 }
