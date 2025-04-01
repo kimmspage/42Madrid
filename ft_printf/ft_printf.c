57d2896572938c847448b223c3c68d97564ca521
Kimberly Dungaya <kimberlydungaya@Air-de-Kimberly.home>
2025-04-02 00:52:47 +0200

    :)

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,38 +6,51 @@
 /*   By: kimberlydungaya <kimberlydungaya@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 08:07:14 by kimberlydun       #+#    #+#             */
-/*   Updated: 2025/04/01 23:20:13 by kimberlydun      ###   ########.fr       */
+/*   Updated: 2025/04/02 00:44:39 by kimberlydun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdarg.h>
+#include <stddef.h>
 
-/* Manejo especificadores (Corrected Javi and Eddy) */
+/* Manejo de especificadores hexadecimales */
+static int	handle_hex(va_list args, char format, size_t *count)
+{
+	char	*base;
+
+	if (format == 'x')
+		base = "0123456789abcdef";
+	else
+		base = "0123456789ABCDEF";
+	return (ft_puthex(va_arg(args, unsigned int), count, base));
+}
+
+/* Manejo de todos los especificadores */
 int	ft_process_format(va_list args, char format, size_t *count)
 {
-	int				len;
-	unsigned long	addr;
+	void	*ptr;
 
-	len = 0;
 	if (format == 'c')
 		return (ft_putchar(va_arg(args, int), count));
-	else if (format == 's')
+	if (format == 's')
 		return (ft_putstr(va_arg(args, char *), count));
-	else if (format == 'd' || format == 'i')
+	if (format == 'd' || format == 'i')
 		return (ft_putnbr(va_arg(args, int), count));
-	else if (format == 'u')
+	if (format == 'u')
 		return (ft_putunbr(va_arg(args, unsigned int), count));
-	else if (format == 'x' || format == 'X')
-		return (ft_puthex(va_arg(args, unsigned int), count,
-				(format == 'x') ? "0123456789abcdef" : "0123456789ABCDEF"));
-	else if (format == 'p')
+	if (format == 'p')
 	{
-		addr = (unsigned long)va_arg(args, void *);
-		len += ft_putstr("0x", count);
-		len += ft_puthex(addr, count, "0123456789abcdef");
-		return (len);
+		ptr = va_arg(args, void *);
+		if (!ptr)
+			return (ft_putstr("(nil)", count));
+		ft_putstr("0x", count);
+		ft_puthex((unsigned long)ptr, count, "0123456789abcdef");
+		return (*count);
 	}
-	else if (format == '%')
+	if (format == 'x' || format == 'X')
+		return (handle_hex(args, format, count));
+	if (format == '%')
 		return (ft_putchar('%', count));
 	return (0);
 }
@@ -55,7 +68,8 @@ int	ft_printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			ft_process_format(args, *format, &count);
+			if (*format)
+				ft_process_format(args, *format, &count);
 		}
 		else
 			ft_putchar(*format, &count);
--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -6,72 +6,64 @@
 /*   By: kimberlydungaya <kimberlydungaya@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 08:36:12 by kimberlydun       #+#    #+#             */
-/*   Updated: 2025/04/01 23:20:19 by kimberlydun      ###   ########.fr       */
+/*   Updated: 2025/04/02 00:26:41 by kimberlydun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 /*Auxiliar de Carácter*/
+int	ft_putchar(char c, size_t *count)
+{
+	if (write(1, &c, 1) == -1)
+		return (-1);
+	(*count)++;
+	return (1);
+}
+
+/*Auxiliar de Cadenita*/
 int	ft_putstr(char *str, size_t *count)
 {
-    size_t initial_count = *count;
-
-    if (!str)
-        str = "(null)";
-    while (*str)
-    {
-        ft_putchar(*str, count);
-        str++;
-    }
-    return (*count - initial_count);
+	if (!str)
+		str = "(null)";
+	while (*str)
+	{
+		if (ft_putchar(*str++, count) == -1)
+			return (-1);
+	}
+	return (*count);
 }
 
-/* Auxiliar cadena */
-int	ft_putunbr(unsigned int n, size_t *count)
-{
-    if (n >= 10)
-        ft_putunbr(n / 10, count);
-    ft_putchar(n % 10 + '0', count);
-    return (*count);
-}
-
-/* Auxiliar número en base 10 */
+/* Auxiliar número de base 10*/
 int	ft_putnbr(int n, size_t *count)
 {
 	if (n == -2147483648)
-	{
-		ft_putstr("-2147483648", count);
-		return (11); /* Length of "-2147483648" */
-	}
+		return (ft_putstr("-2147483648", count));
 	if (n < 0)
 	{
 		ft_putchar('-', count);
 		n = -n;
-		(*count)++;
 	}
 	if (n >= 10)
-	{
 		ft_putnbr(n / 10, count);
-	}
-	ft_putchar('0' + n % 10, count);
-	(*count)++;
+	ft_putchar((n % 10) + '0', count);
 	return (*count);
 }
 
-/* Auxiliar para número sin signo en base 10 */
+/* Auxiliar número sin signo en base 10 */
 int	ft_putunbr(unsigned int n, size_t *count)
 {
 	if (n >= 10)
 		ft_putunbr(n / 10, count);
-	ft_putchar(n % 10 + '0', count);
+	ft_putchar((n % 10) + '0', count);
+	return (*count);
 }
 
 /* Auxiliar en base hexadecimal */
 int	ft_puthex(unsigned int n, size_t *count, char *base)
 {
-    if (n >= 16)
-        ft_puthex(n / 16, count, base);
-    ft_putchar(base[n % 16], count);
-    return (*count);
+	if (n >= 16)
+		ft_puthex(n / 16, count, base);
+	ft_putchar(base[n % 16], count);
+	return (*count);
 }
